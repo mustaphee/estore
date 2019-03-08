3a3d0fb3600084683a5530fc7a5cde7b62fc54ee
Yusuff Mustapha <mustaphee94@gmail.com>
2019-03-08 08:38:25 +0100

    added the global components and ajax plugin

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import FooterComp from '@/components/partials/Footer'
+import HeaderComp from '@/components/partials/Header'
 
 Vue.config.productionTip = false
+
+Vue.component('header-comp', HeaderComp)
+Vue.component('footer-comp', FooterComp)
+
+// Create an axios instance with the baseURL initialized
+const superAxios = axios.create({
+  baseURL: 'https://backendapi.turing.com/'
+})
+// Set the default Content-Type for every request
+superAxios.defaults.headers.common['Content-Type'] = 'application/json'
+// Create a global prototypal instance of axios with the above instance
+Vue.prototype.ajax = superAxios
 
 /* eslint-disable no-new */
 new Vue({
