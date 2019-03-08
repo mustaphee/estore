128466b8216d2b5e42f87c99e17f1222381597e9
Yusuff Mustapha <mustaphee94@gmail.com>
2019-03-08 08:39:20 +0100

    Created Header, Footer, Menu for the homepage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,256 @@
+<template>
+ <div id="all">    
+      <header-comp></header-comp>
+      <div id="heading-breadcrumbs">
+        <div class="container">
+          <div class="row d-flex align-items-center flex-wrap">
+            <div class="col-md-7">
+              <h1 class="h2 text-center">Welcome to Our Store</h1>
+            </div>
+            <!-- <div class="col-md-5">
+              <ul class="breadcrumb d-flex justify-content-end">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item active">Category with left sidebar</li>
+              </ul>
+            </div> -->
+          </div>
+        </div>
+      </div>
+
+       <section>
+            <div class="project owl-carousel">
+              <div class="item"><img src="static/img/main-slider1.jpg" alt="" class="img-fluid"></div>
+              <div class="item"><img src="static/img/main-slider2.jpg" alt="" class="img-fluid"></div>
+              <div class="item"><img src="static/img/main-slider3.jpg" alt="" class="img-fluid"></div>
+              <div class="item"><img src="static/img/main-slider4.jpg" alt="" class="img-fluid"></div>
+            </div>
+          </section>
+
+      <div id="content">
+        <div class="container">
+          <div class="row bar">
+            <div class="col-md-3">
+              <!-- MENUS AND FILTERS-->
+              <div class="panel panel-default sidebar-menu">
+                <div class="panel-heading">
+                  <h3 class="h4 panel-title">Categories</h3>
+                </div>
+                <div class="panel-body">
+                  <ul class="nav nav-pills flex-column text-sm category-menu">
+                    <li class="nav-item"><a href="shop-category.html" class="nav-link d-flex align-items-center justify-content-between"><span>Men </span><span class="badge badge-secondary">42</span></a>
+                      <ul class="nav nav-pills flex-column">
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">T-shirts</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Shirts</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Pants</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Accessories</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item"><a href="shop-category.html" class="nav-link active d-flex align-items-center justify-content-between"><span>Ladies  </span><span class="badge badge-light">123</span></a>
+                      <ul class="nav nav-pills flex-column">
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">T-shirts</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Skirts</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Pants</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Accessories</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item"><a href="shop-category.html" class="nav-link d-flex align-items-center justify-content-between"><span>Kids  </span><span class="badge badge-secondary">11</span></a>
+                      <ul class="nav nav-pills flex-column">
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">T-shirts</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Skirts</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Pants</a></li>
+                        <li class="nav-item"><a href="shop-category.html" class="nav-link">Accessories</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="panel panel-default sidebar-menu">
+                <div class="panel-heading d-flex align-items-center justify-content-between">
+                  <h3 class="h4 panel-title">Brands</h3><a href="#" class="btn btn-sm btn-danger"><i class="fa fa-times-circle"></i><span class="d-none d-md-inline-block">Clear</span></a>
+                </div>
+                <div class="panel-body">
+                  <form>
+                    <div class="form-group">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Armani  (10)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Versace  (12)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Carlo Bruni  (15)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Jack Honey  (14)
+                        </label>
+                      </div>
+                    </div>
+                    <button class="btn btn-sm btn-template-outlined"><i class="fa fa-pencil"></i> Apply</button>
+                  </form>
+                </div>
+              </div>
+              <div class="panel panel-default sidebar-menu">
+                <div class="panel-heading d-flex align-items-center justify-content-between">
+                  <h3 class="h4 panel-titlen">Colours</h3><a href="#" class="btn btn-sm btn-danger"><i class="fa fa-times-circle"></i><span class="d-none d-md-inline-block">Clear</span></a>
+                </div>
+                <div class="panel-body">
+                  <form>
+                    <div class="form-group">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"><span class="colour white"></span> White (14)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"><span class="colour blue"></span> Blue (10)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"><span class="colour green"></span>  Green (20)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"><span class="colour yellow"></span>  Yellow (13)
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"><span class="colour red"></span>  Red (10)
+                        </label>
+                      </div>
+                    </div>
+                    <button class="btn btn-sm btn-template-outlined"><i class="fa fa-pencil"></i> Apply</button>
+                  </form>
+                </div>
+              </div>
+              <div class="banner"><a href="shop-category.html"><img src="static/img/banner.jpg" alt="sales 2014" class="img-fluid"></a></div>
+            </div>
+            <div class="col-md-9">
+              <p class="text-muted lead">In our Ladies department we offer wide selection of the best products we have found and carefully selected worldwide. Pellentesque habitant morbi tristique senectus et netuss.</p>
+              <div class="row products products-big">
+                <div class="col-lg-4 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="static/img/product1.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">Fur coat with very but very very long name</a></h3>
+                      <p class="price">$143.00</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="static/img/product2.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">White Blouse Armani</a></h3>
+                      <p class="price">
+                        <del>$280</del> $143.00
+                      </p>
+                    </div>
+                    <div class="ribbon-holder">
+                      <div class="ribbon sale">SALE</div>
+                      <div class="ribbon new">NEW</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="static/img/product3.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">Black Blouse Versace</a></h3>
+                      <p class="price">$143.00</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="static/img/product4.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">Black Blouse Versace</a></h3>
+                      <p class="price">$143.00</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="static/img/product3.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">White Blouse Armani</a></h3>
+                      <p class="price">
+                        <del>$280</del> $143.00
+                      </p>
+                    </div>
+                    <div class="ribbon-holder">
+                      <div class="ribbon sale">SALE</div>
+                      <div class="ribbon new">NEW</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="static/img/product4.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">White Blouse Versace</a></h3>
+                      <p class="price">$143.00</p>
+                    </div>
+                    <div class="ribbon-holder">
+                      <div class="ribbon new">NEW</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 banner mb-small"><a href="#"><img src="static/img/banner2.jpg" alt="" class="img-fluid"></a></div>
+              </div>
+              <div class="pages">
+                <p class="loadMore text-center"><a href="#" class="btn btn-template-outlined"><i class="fa fa-chevron-down"></i> Load more</a></p>
+                <nav aria-label="Page navigation example" class="d-flex justify-content-center">
+                  <ul class="pagination">
+                    <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-double-left"></i></a></li>
+                    <li class="page-item active"><a href="#" class="page-link">1</a></li>
+                    <li class="page-item"><a href="#" class="page-link">2</a></li>
+                    <li class="page-item"><a href="#" class="page-link">3</a></li>
+                    <li class="page-item"><a href="#" class="page-link">4</a></li>
+                    <li class="page-item"><a href="#" class="page-link">5</a></li>
+                    <li class="page-item"><a href="#" class="page-link"><i class="fa fa-angle-double-right"></i></a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- GET IT-->
+      <div class="get-it">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 text-center p-3">
+              <h3>Do you want cool website like this one?</h3>
+            </div>
+            <div class="col-lg-4 text-center p-3">   <a href="#" class="btn btn-template-outlined-white">Buy this template now</a></div>
+          </div>
+        </div>
+      </div>
+      <footer-comp></footer-comp>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
