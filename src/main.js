228594742507a7a309dea1b2c65ced0373293d73
Yusuff Mustapha <mustaphee94@gmail.com>
2019-03-09 16:46:40 +0100

    Added Vuex for state management and created a store

--- src/main.js
+++ src/main.js
@@ -4,17 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import { store } from './stores'
 import FooterComp from '@/components/partials/Footer'
 import HeaderComp from '@/components/partials/Header'
 
 Vue.config.productionTip = false
-
 Vue.component('header-comp', HeaderComp)
 Vue.component('footer-comp', FooterComp)
 
+// Get the base URl from the environment
+// const API_URL = process.env.API_BASE_URL
+const API_URL = 'https://backendapi.turing.com/'
 // Create an axios instance with the baseURL initialized
 const superAxios = axios.create({
-  baseURL: 'https://backendapi.turing.com/'
+  baseURL: API_URL
 })
 // Set the default Content-Type for every request
 superAxios.defaults.headers.common['Content-Type'] = 'application/json'
@@ -25,6 +28,7 @@ Vue.prototype.ajax = superAxios
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
